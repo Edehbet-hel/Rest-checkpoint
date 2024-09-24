32390c144635cfc727f56a2efd9357e5f7e4f04a
Bethel <bethelededh668@gmail.com>
2024-09-24 15:16:25 -0700

    initial commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createUser, deleteUser, getUsers, updateUser } from "../controllers/UserControllers.js";
+const userRouter = express.Router();
+userRouter.route("/")
+.post(createUser)
+.get(getUsers);
+userRouter.route("/:id")
+.put(updateUser)
+.delete(deleteUser);
+export default userRouter;
